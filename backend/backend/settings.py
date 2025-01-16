cec1c6bb3c2294ffae3124427e6e24b23ead9dfe
Alexander Ivanisov <AlexUdganda@yandex.ru>
2025-01-16 22:08:42 +0300

    Add Actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,9 +1,7 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
@@ -11,8 +9,7 @@ DEBUG = True
 
 # Кроме того, в список ALLOWED_HOSTS в файле settings.py необходимо добавить
 # IP-адрес и (если есть) доменное имя вашего сервера.
-ALLOWED_HOSTS = ['127.0.0.1','89.169.169.185', 'taski.codeexp.ru']
-
+ALLOWED_HOSTS = ['127.0.0.1', '89.169.169.185', 'taski.codeexp.ru']
 
 # Application definition
 
@@ -59,7 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -75,7 +71,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -95,7 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -108,7 +102,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
